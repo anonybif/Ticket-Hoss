e0bb671077f52bded30db943f38cad9f1389609f
anonybif <94902441+anonybif@users.noreply.github.com>
2023-02-04 02:05:07 +0300

    added search bar,search btn and filter icon

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
-import * as React from "react"
+import * as React from "react";
+import "../styles/global.css";
+import Navbar from "../components/navbar";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter, faFilterCircleDollar, faFilterCircleXmark } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <Navbar />
+      <div className="home">
+        <div className="header"></div>
+        <div className="search">
+          <div className="searchBar">
+            <input type="text" placeholder="Search Tracks" />
+            <button>Search</button>
+          </div>
+          <span className="filterBtn"><FontAwesomeIcon icon={faFilter}></FontAwesomeIcon></span>
+        </div>
+      </div>
+    </>
+  )
 }
